706a0f4a089520d9906ff6206f543681b02a1a0a
MargaLutic <GoliadkinaMN21@st.ithub.ru>
2023-12-14 14:34:21 +0300

    Exclude virtual environment and update line endings

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from teest.src.your_api import YourAPIClient
+
+
+
+@pytest.fixture
+def api_client(request):
+    base_url = request.config.getoption("--baseurl")
+    return YourAPIClient(base_url)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--baseurl", action="store", default="http://myopencart.example.com/index.php?route=api/cart/add",
+                     help="Base URL for API testing")
